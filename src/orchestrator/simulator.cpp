09288c4513ae66f308706b341349719c3f970e5b
francesco.barbarulo <barbarulofrancesco@gmail.com>
2019-04-12 11:43:12 +0200

    Added main bus and CPU and MEM modules for simulation

--- src/orchestrator/simulator.cpp
+++ src/orchestrator/simulator.cpp
@@ -1,4 +1,5 @@
 #include "ExampleModule.h"
+#include "CPUTest.h"
 #include "../memory/memory.h"
 #include "System.h"
 
@@ -6,11 +7,15 @@ using namespace std;
 
 int main() {
 	System system;
+	Bus main_bus = Bus();
 
 	//Initialize the system here by adding modules to the system object
-	system.addModule(new ExampleModule("Alice"));
-	system.addModule(new ExampleModule("Bob", 10));//The second parameter is the priority. It's useful only when you need to intercept someone's else message
-	system.addModule(new Memory("MEM", 0));
+
+	//system.addModule(new ExampleModule("Alice"));
+	//system.addModule(new ExampleModule("Bob", 10));//The second parameter is the priority. It's useful only when you need to intercept someone's else message
+
+	system.addModule(new CPUTest("CPU", 0, &main_bus));
+	system.addModule(new Memory("MEM", 0, &main_bus));
 	//Call run() to start the simulation
 	system.run();
 }
