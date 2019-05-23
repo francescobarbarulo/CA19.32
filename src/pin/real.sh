7d373ca5f86b7b5e9efc235cc902228a991ae56f
giomba <giomba@glgprograms.it>
2019-05-23 17:04:07 +0200

    [test] added full instruction+data streams test

--- src/pin/real.sh
+++ src/pin/real.sh
@@ -5,3 +5,5 @@ CMD=$1
 ./pin -t source/tools/SimpleExamples/obj-intel64/pinatrace.so -- $CMD
 cat pinatrace.out | awk --non-decimal-data '{printf "R 0x%x\r\n", $1 % 0xffff}' | tail -n +4 | head -n -1 > ../examples/$CMD.i.txt
 cat pinatrace.out | awk --non-decimal-data '{printf "%s 0x%x\r\n", $2, $3 % 0xffff}' | tail -n +4 | head -n -1 > ../examples/$CMD.d.txt
+cat pinatrace.out | awk --non-decimal-data '{printf "R 0x%x\r\n%s 0x%x\r\n", $1 % 0xffff, $2, $3 %0xffff}' | tail -n +4 | head -n -1 > ../examples/$CMD.t.txt
+
